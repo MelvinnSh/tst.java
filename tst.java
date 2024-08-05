6a6a8d9e401a576e0a12032dbeb6d2d8e9a22382
MelvinnSh <melvin_ismiananta_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:44:39 +0700

    Add files via upload

---
+++ tst.java
@@ -0,0 +1,6 @@
+public class tst {
+    public static void main(String[] args) {
+        System.out.println("Nama saya melvin andrea daari kelas XRPL7 dari malang");
+        String a = "1";
+    }
+}
